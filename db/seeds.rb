f52287515bdfdaf3201ddf59a747f63c49b534cf
yan_ramanovich@epam.com <yan_ramanovich@epam.com>
2024-08-24 12:04:05 +0200

    Fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-unless Admin.where(email: "sasha@esasha.com").exists?
+unless Admin.where(email: "sasha@sasha.com").exists?
   Admin.create!(email: "sasha@sasha.com", password: "sasha@sasha.com", password_confirmation: "sasha@sasha.com")
 end
